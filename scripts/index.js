dc971b2a30d7a046de87ff2d6d0e830dbac1e472
nalinpandey <nalin456pandey@gmail.com>
2022-06-11 18:25:13 +0530

    CHECKPOINT 11

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,26 @@ var element = document.getElementById("viewless");
 function expand() {
     var x = document.getElementById('none');
     if (x.style.display === 'none') {
-        element.innerHTML="View less"
+        element.innerHTML="View Less"
       x.style.display = 'block';
     } else {
         element.innerHTML="View More"
       x.style.display = 'none';
     }
   }
+
+ function login(){
+    var xhttp= new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        console.log(this.readyState);
+        if(this.readyState===4 && this.status===200){
+            var data=this.response;
+            var Jdata=JSON.parse(data); 
+            alert("login Successfully");
+        }else{
+            alert("Invalid login");
+        }
+ }
+ xhttp.open('GET',"login.txt", true);
+ xhttp.send();
+}
